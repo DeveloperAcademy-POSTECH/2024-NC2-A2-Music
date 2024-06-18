40f631c6b462f29c7277eca7c4b34552bc7c29b7
YuBeen <87077859+dbqls200@users.noreply.github.com>
2024-06-18 15:00:12 +0900

    Create new project

---
+++ OnAir/OnAir/OnAirApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnAirApp.swift
+//  OnAir
+//
+//  Created by 김유빈 on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct OnAirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
